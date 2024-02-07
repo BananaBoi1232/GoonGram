76817c12ac58a3c3164cf10cd041fb0d3f77ce71
BananaBoi1232 <martin1420z@comcast.net>
2024-02-07 16:16:08 -0500

    literally everything

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,73 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/bannedUsers', function () {
+    return view('bannedUsers');
+});
+
+Route::get('/blockedUsers', function () {
+    return view('blockedUsers');
+});
+
+Route::get('/createPost', function () {
+    return view('createPost');
+});
+
+
+Route::get('/directMessage', function () {
+    return view('directMessage');
+});
+
+
+Route::get('/friends', function () {
+    return view('friends');
+});
+
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+
+Route::get('/manageAccount', function () {
+    return view('manageAccount');
+});
+
+
+Route::get('/messages', function () {
+    return view('messages');
+});
+
+
+Route::get('/otherAccount', function () {
+    return view('otherAccount');
+});
+
+
+Route::get('/personalAccount', function () {
+    return view('personalAccount');
+});
+
+Route::get('/reportedPosts', function () {
+    return view('reportedPosts');
+});
+
+
+Route::get('/search', function () {
+    return view('search');
+});
+
+
+Route::get('/settings', function () {
+    return view('settings');
+});
+
+
+Route::get('/signup', function () {
+    return view('signup');
+});
+
+
