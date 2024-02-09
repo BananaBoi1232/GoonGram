c7406e515a4c6d381d96209cbd003965eaffeefb
BananaBoi1232 <martin1420z@comcast.net>
2024-02-09 12:44:07 -0500

    Created Api controller for accounts and added bootstrap files

---
+++ app/Models/account_controller_api.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class account_controller_api extends Model
+{
+    use HasFactory;
+}
