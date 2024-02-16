b3a0ddf23b9efaba26bc088c08c5606f24478b09
Endermage44 <bad0529@icloud.com>
2024-02-16 12:32:09 -0500

    Added navbars to all pages

--- resources/views/manageAccount.blade.php
+++ resources/views/manageAccount.blade.php
@@ -7,8 +7,9 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Manage Account</title>
+    @include('navbar')
 </head>
-<body class="w-100 h-75 d-flex justify-content-center align-items-center">
+<body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <div class="d-flex flex-column">
 
--- resources/views/reportedPosts.blade.php
+++ resources/views/reportedPosts.blade.php
@@ -8,34 +8,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <title>Reported Posts</title>
+    @include('navbar')
 </head>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark navbar-fixed-top">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="/">GoonGram</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="/">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/search">Search</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/createPost">Create Post</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/personalAccount">Personal Account</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/settings">Account Settings</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
 <body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
