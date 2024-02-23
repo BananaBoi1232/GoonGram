1cccdb485a0bd165a05092fc88881efc7a0dcc7e
Endermage44 <bad0529@icloud.com>
2024-02-23 15:58:33 -0500

    Added page login checks and logout functionality

--- app/Http/Controllers/LogoutController.php
+++ app/Http/Controllers/LogoutController.php
@@ -5,11 +5,12 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+use Illuminate\Support\Facades\Cache;
 class LogoutController extends Controller
 {
     public function logout(Request $request){
         Auth::logout();
+        Cache::flush();
         return redirect("/");
     }
 }
