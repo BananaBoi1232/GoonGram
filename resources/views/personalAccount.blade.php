15213b40642d7cd42eae46e1613360e8b1065181
oscar <huehuehuehuehue1234@gmail.com>
2024-05-03 11:51:59 -0400

    Create Post, Personal Account, Manage Acccount, and search page CSS nearly completed. will make them look prettier. Need to make sure eveything still works

--- resources/views/personalAccount.blade.php
+++ resources/views/personalAccount.blade.php
@@ -1,114 +1,76 @@
 <!DOCTYPE html>
-<html lang="en" class="w-100 h-100">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name = "csrf-token" content="{{ csrf_token() }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Your Account</title>
+    @include('navbar')
 
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-    
-    <title>Your Account</title>
-    @include('navbar')
+    <link rel="stylesheet" href="{{ asset('css/personalAccount.css') }}">
 </head>
 <body>
-    <div class = "d-flex justify-content-center flex-column">
-
+    <div class="d-flex justify-content-center flex-column">
         <div class="d-flex">
-
             <div>
-
                 @php($profilePicture = $user->profilePicture)
-                <img draggable="false" id = "profilePicture" name = "profilePicture" style = "height:350px; width:350px;" 
-                    src="@if($profilePicture == null) {{ asset('storage/avatar-3814049_1920.png') }} 
-                            @else {{ asset('storage/'.$profilePicture) }}
-                @endif">
-
+                <img draggable="false" id="profilePicture" name="profilePicture" class="profile-picture"
+                    src="@if($profilePicture == null) {{ asset('storage/avatar-3814049_1920.png') }}
+                          @else {{ asset('storage/'.$profilePicture) }} @endif">
             </div>
-
-            <div class="d-flex flex-column p-2">
-
+            <div class="d-flex flex-column p-2 user-info">
                 <div>
-                    <h1>{{ $user->username }}</h1>
+                    <h1 style="color: white;">{{ $user->username }}</h1>
                 </div>
-
-                <div class = "d-flex flex-row">
-                    <div style = "font-size:16px">{{ $user->followerCount }} followers</div>
-                    <div class = "ps-4" style = "font-size:16px">{{ $user->followingCount }} following</div>
+                <div class="d-flex flex-row">
+                    <div style="font-size:16px">{{ $user->followerCount }} Followers</div>
+                    <div class="ps-4" style="font-size:16px">{{ $user->followingCount }} Following</div>
                 </div>
-
                 <div>
-                    <textarea style="height:250px; width:600px; resize:none; border:none; outline:none;" class = "pt-4" readonly>{{ $user->bio }}</textarea>
+                    <textarea style="height:250px; width:600px; resize:none; border:none; outline:none; background-color: transparent; color: white;" class="pt-4" readonly>{{ $user->bio }}</textarea>
                 </div>
-
             </div>
-
         </div>
-
         <hr>
-
-        <div d-flex class = "flex-column justify-content-center text-align-center">
-            
-            <div class = "d-flex justify-content-center text-align-center">
-                <h2>posts</h2>
+        <div d-flex class="flex-column justify-content-center text-align-center">
+            <div class="d-flex justify-content-center text-align-center">
+                <h2 class="post-header">Posts</h2>
             </div>
-
-            <div class = "d-flex flex-row flex-wrap">
-
+            <div class="d-flex flex-row flex-wrap">
                 @foreach($posts as $post)
-
-                    <img draggable="false" src = "{{ asset('storage/'.$post->postImage) }}" style = "height:190px; width:190px;" class = "p-1" data-bs-toggle="modal" data-bs-target="#image{{ $post->postID }}">
-                    
+                    <img draggable="false" src="{{ asset('storage/'.$post->postImage) }}" class="post-image p-1"
+                        data-bs-toggle="modal" data-bs-target="#image{{ $post->postID }}">
                     <div class="modal fade" id="image{{ $post->postID }}">
-                        
-                        <div class = "postID d-none">{{ $post->postID }}</div>
-                        
+                        <div class="postID d-none">{{ $post->postID }}</div>
                         <div class="modal-dialog">
-                            
-                            <div class="modal-content">     
-                                
+                            <div class="modal-content">
                                 <div class="modal-header d-flex justify-content-center">
-                                    
-                                    <img draggable="false" src = "{{ asset('storage/'.$post->postImage) }}" style = "height:420px; width:420px;" class = "p-1">
+                                    <img draggable="false" src="{{ asset('storage/'.$post->postImage) }}" class="p-1" style="height:420px; width:420px;">
                                     <button type="button" class="btn-close position-absolute top-0 end-0 m-1" data-bs-dismiss="modal"></button>
-                                
                                 </div>
-                                
                                 <div class="modal-body">
-                                
-                                    <div class="caption">{{ $post->caption }}</div>    
-                                    
+                                    <div class="caption">{{ $post->caption }}</div>
                                     <div class="likes">
-
-                                        <a class = "likeBtn">
-                                            <ion-icon name="thumbs-up-outline" class="likeIcon p-1  @if($liked->contains($post->postID)) text-warning @endif" style = "width:35px; height:35px; color:black;"></ion-icon>
+                                        <a class="likeBtn">
+                                            <ion-icon name="thumbs-up-outline" class="likeIcon p-1 @if($liked->contains($post->postID)) text-warning @endif" style="width:35px; height:35px; color:black;"></ion-icon>
                                         </a>
-
-                                        <div class = "likeCount">{{ $post->likeCount }} Likes </div>
-
+                                        <div class="likeCount">{{ $post->likeCount }} Likes</div>
                                     </div>
-                                    
                                 </div>
-                            
                             </div>
-                        
-                        </div>                                
-                        
+                        </div>
                     </div>
-
                 @endforeach
-
             </div>
-
         </div>
-
     </div>
     <script>
         $(document).ready(function(){
-            
+
             $(".likeBtn").click(function(e){
 
                 e.preventDefault();
@@ -140,7 +102,7 @@
                             $(likeIcon).removeClass('text-warning');
                         }
                     }
-                }); 
+                });
             });
         });
     </script>
--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -7,6 +7,7 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="{{ asset('css/search.css') }}">
     <title>Search</title>
     @include('navbar')
 
@@ -37,58 +38,41 @@
 </head>
 <body>
     {{-- SEARCH BAR --}}
-    <div class="container align-items-center">
-        <div class="row justify-content-center">
-            <div class="col-auto gy-3">
-                <form class="input-group mb-3 col-sm">
-                    <input type="text" class="form-control" placeholder="Search users, posts, etc..." aria-label="Search users, posts, etc..." aria-describedby="button-addon2" name="search">
-                    <div class="input-group-append">
-                        <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Search</button>
-                    </div>
-                </form>
+    <div class="container">
+        <div class="search-container">
+            <form class="input-group mb-3 col-sm">
+                <input type="text" class="form-control" placeholder="Search users, posts, etc..." aria-label="Search users, posts, etc..." aria-describedby="button-addon2" name="search">
+                <div class="input-group-append">
+                    <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Search</button>
+                </div>
+            </form>
+        </div>
+
+        {{-- SHOW DATA FROM SEARCH --}}
+        <div class="d-flex flex-column justify-content-start">
+            <h2>Users</h2>
+            <div>
+                @foreach($searchUsers as $useres)
+                    <div class="userRes"><a href="/otherAccount/{{ $useres->id }}">{{ $useres -> username }}</a></div>
+                @endforeach
             </div>
         </div>
-    </div>
-    
-    {{-- SHOW DATA FROM SEARCH --}}
-    <div class="container">
-        <div style="overflow-y: auto; width 100%; height 100px">
-            <table>
-                <tr>
-                    <th>Users</th>
-                </tr>
-                <tr class="d-flex flex-row flex-wrap">
-                @foreach($searchUsers as $useres)
-                    <th class="userRes"><a href="/otherAccount/{{ $useres->id }}">{{ $useres -> username }}</a></th>
-                @endforeach
-                </tr>
-                
-            </table>
-        </div>
-        <div style="overflow-y: auto; width 100%; height 100px">
-            <table>
-                <tr>
-                    <th>Tags</th>
-                </tr>
-                <tr class="d-flex flex-row flex-wrap">
+        <div class="d-flex flex-column justify-content-center">
+            <h2>Tags</h2>
+            <div>
                 @foreach($searchTags as $tagres)
-                    <th class="userRes">{{ $tagres -> tagName }}</th>
+                    <div class="userRes">{{ $tagres -> tagName }}</div>
                 @endforeach
-                </tr>
-            </table>
+            </div>
         </div>
-        <div style="overflow-y: auto; width 100%; height 200px">
-            <table>
-                <tr>
-                    <th>Posts</th>
-                </tr>
-                <tr class="d-flex flex-row flex-wrap">
+        <div class="d-flex flex-column justify-content-end">
+            <h2>Posts</h2>
+            <div>
                 @foreach($searchPosts as $postres)
-                    <th class="userRes caption">{{ $postres -> caption }}</th>
-                    <th class="image"><img src="{{ asset('storage/'.$postres -> postImage) }}" style = "height:190px; width:190px;" alt=""></th>
+                    <div class="userRes caption">{{ $postres -> caption }}</div>
+                    <div class="image"><img src="{{ asset('storage/'.$postres -> postImage) }}" style="height: 190px; width: 190px;" alt=""></div>
                 @endforeach
-                </tr>
-            </table>
+            </div>
         </div>
     </div>
 </body>
